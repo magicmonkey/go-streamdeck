19fce65b5a4ebd886fdf3ae5e7b9668a4df5b3a6
Kevin <kevin@magicmonkey.org>
2020-06-13 20:57:57 +0100

    Some fixes from the feedback from go lint

--- buttons/imagefile.go
+++ buttons/imagefile.go
@@ -8,6 +8,8 @@ import (
 	streamdeck "github.com/magicmonkey/go-streamdeck"
 )
 
+// ImageFileButton represents a button with an image on it, where the image is loaded
+// from a file.
 type ImageFileButton struct {
 	filePath      string
 	img           image.Image
@@ -16,18 +18,22 @@ type ImageFileButton struct {
 	actionHandler streamdeck.ButtonActionHandler
 }
 
+// GetImageForButton is the interface implemention to get the button's image as an image.Image
 func (btn *ImageFileButton) GetImageForButton() image.Image {
 	return btn.img
 }
 
+// SetButtonIndex is the interface implemention to set which button on the Streamdeck this is
 func (btn *ImageFileButton) SetButtonIndex(btnIndex int) {
 	btn.btnIndex = btnIndex
 }
 
+// GetButtonIndex is the interface implemention to get which button on the Streamdeck this is
 func (btn *ImageFileButton) GetButtonIndex() int {
 	return btn.btnIndex
 }
 
+// SetFilePath allows the image file to be changed on the fly
 func (btn *ImageFileButton) SetFilePath(filePath string) error {
 	btn.filePath = filePath
 	err := btn.loadImage()
@@ -60,20 +66,27 @@ func (btn *ImageFileButton) loadImage() error {
 	return nil
 }
 
+// RegisterUpdateHandler is the interface implemention to let the engine give this button a callback to
+// use to request that the button image is updated on the Streamdeck.
 func (btn *ImageFileButton) RegisterUpdateHandler(f func(streamdeck.Button)) {
 	btn.updateHandler = f
 }
 
+// SetActionHandler allows a ButtonActionHandler implementation to be
+// set on this button, so that something can happen when the button is pressed.
 func (btn *ImageFileButton) SetActionHandler(a streamdeck.ButtonActionHandler) {
 	btn.actionHandler = a
 }
 
+// Pressed is the interface implementation for letting the engine notify that the button has been
+// pressed.  This hands-off to the specified ButtonActionHandler if it has been set.
 func (btn *ImageFileButton) Pressed() {
 	if btn.actionHandler != nil {
 		btn.actionHandler.Pressed(btn)
 	}
 }
 
+// NewImageFileButton creates a new ImageFileButton with the specified image on it
 func NewImageFileButton(filePath string) (*ImageFileButton, error) {
 	btn := &ImageFileButton{filePath: filePath}
 	err := btn.loadImage()
