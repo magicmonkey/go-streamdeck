a80e08f792f53481f94329d660986fe702e88768
Lorna Jane Mitchell <lorna@lornajane.net>
2020-06-20 18:12:51 +0100

    Add some comments to remaining example files

--- examples/test/test.go
+++ examples/test/test.go
@@ -10,23 +10,25 @@ import (
 )
 
 func main() {
+	// connect to a streamdeck
 	sd, err := streamdeck.Open()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Found device [%s]\n", sd.GetName())
 
+	// remove all button content
 	sd.ClearButtons()
 
+	// manage brightness level
 	sd.SetBrightness(50)
 
+	// show text in increasing lengths on three buttons
 	sd.WriteTextToButton(2, "Hi!", color.RGBA{0, 0, 0, 255}, color.RGBA{0, 255, 255, 255})
 	sd.WriteTextToButton(3, "Hi again!", color.RGBA{0, 0, 0, 255}, color.RGBA{0, 255, 255, 255})
 	sd.WriteTextToButton(4, "Hi again again!", color.RGBA{0, 0, 0, 255}, color.RGBA{0, 255, 255, 255})
 
-	//sd.WriteImageToButton(9, "something.png")
-	//sd.WriteImageToButton(10, "play.jpg")
-	//sd.WriteColorToButton(color.RGBA{255, 0, 255, 0}, 1)
+	// when any button is pressed, clear all buttons, and set an image on the pressed button
 	sd.ButtonPress(func(btnIndex int, sd *streamdeck.Device, err error) {
 		if err != nil {
 			panic(err)
@@ -35,6 +37,7 @@ func main() {
 		sd.WriteImageToButton(btnIndex, "examples/test/play.jpg")
 	})
 
+	// keep the program running
 	var wg sync.WaitGroup
 	wg.Add(1)
 	wg.Wait()
